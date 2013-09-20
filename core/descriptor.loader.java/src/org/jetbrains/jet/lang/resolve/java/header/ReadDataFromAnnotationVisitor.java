4c08daa83e4da967bfa74fe7639ab503b26e6bcd
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2013-09-20 17:23:59 +0400

    Remove hack from AnnotationDescriptorDeserializer
    Now that package fragment classes are generated to "*Package-filename-hash", we
    can use virtualFileFinder to find such classes by FQ name. Support
    KotlinPackageFragment annotation, by which VirtualFileFinder will determine
    that a class was compiled by Kotlin compiler

--- core/descriptor.loader.java/src/org/jetbrains/jet/lang/resolve/java/header/ReadDataFromAnnotationVisitor.java
+++ core/descriptor.loader.java/src/org/jetbrains/jet/lang/resolve/java/header/ReadDataFromAnnotationVisitor.java
@@ -40,6 +40,7 @@ import static org.jetbrains.jet.lang.resolve.java.AbiVersionUtil.isAbiVersionCom
     private enum HeaderType {
         CLASS(JvmAnnotationNames.KOTLIN_CLASS),
         PACKAGE(JvmAnnotationNames.KOTLIN_PACKAGE),
+        PACKAGE_FRAGMENT(JvmAnnotationNames.KOTLIN_PACKAGE_FRAGMENT),
         OLD_CLASS(JvmAnnotationNames.OLD_JET_CLASS_ANNOTATION),
         OLD_PACKAGE(JvmAnnotationNames.OLD_JET_PACKAGE_CLASS_ANNOTATION);
 
@@ -89,6 +90,8 @@ import static org.jetbrains.jet.lang.resolve.java.AbiVersionUtil.isAbiVersionCom
                 return SerializedDataHeader.create(version, annotationData, SerializedDataHeader.Kind.CLASS, fqName);
             case PACKAGE:
                 return SerializedDataHeader.create(version, annotationData, SerializedDataHeader.Kind.PACKAGE, fqName);
+            case PACKAGE_FRAGMENT:
+                return new PackageFragmentClassFileHeader(version, fqName);
             case OLD_CLASS:
             case OLD_PACKAGE:
                 return new OldAnnotationHeader(fqName);
@@ -117,6 +120,9 @@ import static org.jetbrains.jet.lang.resolve.java.AbiVersionUtil.isAbiVersionCom
         if (newType == HeaderType.CLASS || newType == HeaderType.PACKAGE) {
             return kotlinClassOrPackageVisitor(desc);
         }
+        else if (newType == HeaderType.PACKAGE_FRAGMENT) {
+            return kotlinPackageFragmentVisitor(desc);
+        }
 
         return null;
     }
@@ -126,12 +132,7 @@ import static org.jetbrains.jet.lang.resolve.java.AbiVersionUtil.isAbiVersionCom
         return new AnnotationVisitor(Opcodes.ASM4) {
             @Override
             public void visit(String name, Object value) {
-                if (name.equals(JvmAnnotationNames.ABI_VERSION_FIELD_NAME)) {
-                    version = (Integer) value;
-                }
-                else if (isAbiVersionCompatible(version)) {
-                    throw new IllegalStateException("Unexpected argument " + name + " for annotation " + desc);
-                }
+                visitIntValueForSupportedAnnotation(name, value, desc);
             }
 
             @Override
@@ -166,5 +167,24 @@ import static org.jetbrains.jet.lang.resolve.java.AbiVersionUtil.isAbiVersionCom
                 };
             }
         };
+    }
+
+    @NotNull
+    private AnnotationVisitor kotlinPackageFragmentVisitor(final String desc) {
+        return new AnnotationVisitor(Opcodes.ASM4) {
+            @Override
+            public void visit(String name, Object value) {
+                visitIntValueForSupportedAnnotation(name, value, desc);
+            }
+        };
+    }
+
+    private void visitIntValueForSupportedAnnotation(@NotNull String name, @NotNull Object value, @NotNull String desc) {
+        if (name.equals(JvmAnnotationNames.ABI_VERSION_FIELD_NAME)) {
+            version = (Integer) value;
+        }
+        else if (isAbiVersionCompatible(version)) {
+            throw new IllegalStateException("Unexpected argument " + name + " for annotation " + desc);
+        }
     }
 }
